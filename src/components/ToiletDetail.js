295f358d47026064ee51d0033cecd002b5bb244a
vitvara <gridxlgm@gmail.com>
2022-02-12 11:05:44 +0700

    done setup

---
+++ src/components/ToiletDetail.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ToiletDetail = () => {
+  return (
+    <div>ToiletDetail</div>
+  )
+}
+
+export default ToiletDetail
---
+++ src/components/ToiletIconState.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ToiletIconState = () => {
+  return (
+    <div>ToiletIconState</div>
+  )
+}
+
+export default ToiletIconState
