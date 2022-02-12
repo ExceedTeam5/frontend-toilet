b00bedbfa433ee68821a8a2faf5ddf53c69389f8
vitvara <gridxlgm@gmail.com>
2022-02-12 11:25:19 +0700

    toiletLits

--- src/components/ToiletList.js
+++ src/components/ToiletList.js
@@ -1,11 +1,17 @@
-import React from 'react'
-import Toilet from './Toilet'
+import React, { useEffect } from 'react';
+import Toilet from './Toilet';
+import classes from './ToiletList.module.css';
+
+const DUMMY_DATA = {state:[{toiletState: true, timeStamp: 12341},{toiletState: true, timeStamp: 12342},
+  {toiletState: true, timeStamp: 12346}], estimateTime: 123456};
 
 const ToiletList = () => {
-  const DUMMY_DATA = {toiletState: [{toilet1: true}], estimateTime: 12345}
+
+  const listToilet = DUMMY_DATA.state.map((toilet) => <Toilet toiletState={toilet.toiletState} time={toilet.timeStamp} estimateTime={DUMMY_DATA.estimateTime}/>);
   return (
-    {DUMMY_DATA['toiletState'].map()}
-  );
+    <div>{ listToilet }</div>
+    
+  )
 }
 
 export default ToiletList
